bbfcd72be7fe276e2ff0cbc1f13754320896f42d
christopherhanke <christopherhanke@googlemail.com>
2024-07-07 00:36:32 +0200

    getURLsFromHTML function and tests.

--- crawl.test.js
+++ crawl.test.js
@@ -29,16 +29,59 @@ test('normalizeURL capitals https://blog.Boot.dev/path', () => {
     expect(actual).toBe(expected)
 })
 
-test('getURLsfromHTML on http://blog.boot.dev/path', () => {
+test('getURLsfromHTML absolute', () => {
     const inputBody = `
 <html>
     <body>
-        <a href="https://blog.boot.dev">Boot.dev Blog</a>
+        <a href="https://blog.boot.dev/">Boot.dev Blog</a>
     </body>
 </html>
 `
     const inputBase = 'https://blog.boot.dev'
     const actual = getURLsfromHTML(inputBody, inputBase)
-    const expected = ['blog.boot.dev']
-    expect(actual).toBe(expected)
+    const expected = ['https://blog.boot.dev/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML relative', () => {
+    const inputBody = `
+<html>
+    <body>
+        <a href="/path/">Boot.dev Blog</a>
+    </body>
+</html>
+`
+    const inputBase = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(inputBody, inputBase)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML multiple URLS', () => {
+    const inputBody = `
+<html>
+    <body>
+        <a href="/path1/">Boot.dev Blog One</a>
+        <a href="https://blog.boot.dev/path2/">Boot.dev Blog Two</a>
+    </body>
+</html>
+`
+    const inputBase = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(inputBody, inputBase)
+    const expected = ['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML invalid URL', () => {
+    const inputBody = `
+<html>
+    <body>
+        <a href="invalid">Invalid URL</a>
+    </body>
+</html>
+`
+    const inputBase = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(inputBody, inputBase)
+    const expected = []
+    expect(actual).toEqual(expected)
 })
